f349f5bca376ec8dc9a4003d3d88e766c9b08121
MarieChillOut <159060706+MarieChillOut@users.noreply.github.com>
2025-04-05 17:54:31 -0500

    Revert "Lock works"
    This reverts commit 447fe0d6fdead47316bc71031eac65d732b77351.

--- Assets/Scripts/locks/LockScript.cs
+++ Assets/Scripts/locks/LockScript.cs
@@ -9,8 +9,6 @@ public class LockScript : MonoBehaviour
 
     [SerializeField] private int[] correctNums;
     [SerializeField] private TMP_Text[] numTexts;
-    [SerializeField] private bool[] isCorrect = { false, false, false };
-    [SerializeField] private bool isunlocked;
 
     void Start()
     {
@@ -38,22 +36,6 @@ public class LockScript : MonoBehaviour
     private void ChangeText(int x)
     {
         numTexts[x].text = num[x].ToString();
-    }
-
-    public void CheckLock()
-    {
-        Debug.Log(num.Length);
-        for (int i = 0; i <= num.Length - 1 ; i++)
-        {
-            if (num[i] == correctNums[i])
-            {
-                isCorrect[i] = true;
-            }
-            else
-            {
-                isCorrect[i] = false;
-            }
-        }
     }
 
 
