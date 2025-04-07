e5b419a86e0a019f8f9e612b45852798f77f514e
Mason <cweaver@mail.bradley.edu>
2025-04-07 13:55:13 -0500

    AddedAudio

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    [SerializeField] private AudioClip[] audioClips;  // Array to hold audio clips
+    private AudioSource audioSource; // The audio source component to play the clips
+
+    void Start()
+    {
+        // Get the AudioSource component attached to this GameObject
+        audioSource = GetComponent<AudioSource>();
+
+        // Start playing random audio clips
+        PlayRandomAudio();
+    }
+
+    void PlayRandomAudio()
+    {
+        // Pick a random audio clip from the array
+        AudioClip randomClip = audioClips[Random.Range(0, audioClips.Length)];
+
+        // Play the selected audio clip
+        audioSource.clip = randomClip;
+        audioSource.Play();
+
+        // Wait for the current audio clip to finish before playing the next one
+        Invoke("PlayRandomAudio", randomClip.length);
+    }
+}
