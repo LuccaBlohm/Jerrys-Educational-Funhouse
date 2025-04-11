4d91f39ec12aa90b85cb2acc8770ba1b2739ec97
radical-ish <cruzmatt1015@gmail.com>
2025-04-11 17:13:51 -0500

    ui bounds mostly fixed
    may still be mildly buggy but not game breaking anymore

--- Assets/Scripts/WindowUIManager.cs
+++ Assets/Scripts/WindowUIManager.cs
@@ -31,6 +31,7 @@ public class WindowUIManager : MonoBehaviour, IDragHandler,
     {
         parent = gameObject.transform.parent.GetComponent<Canvas>();
         rectTransform = GetComponent<RectTransform>();
+        rectTransformMaxSize = new Vector2(Screen.width, Screen.height);
     }
 
     // allows pop up to affect origin interactable object
@@ -132,7 +133,7 @@ public class WindowUIManager : MonoBehaviour, IDragHandler,
         else if (resizing)
         {
             // sets size
-            if (xBound == bound.none && yBound == bound.none)
+            if (xBound == bound.none || yBound == bound.none)
             {
                 rectTransform.sizeDelta += (eventData.delta * resizeDirection / 2) / parent.scaleFactor;
             }
@@ -140,6 +141,8 @@ public class WindowUIManager : MonoBehaviour, IDragHandler,
 
             bool xMinPassed = rectTransform.sizeDelta.x < rectTransformMinSize.x;
             bool yMinPassed = rectTransform.sizeDelta.y < rectTransformMinSize.y;
+            bool xMaxPassed = rectTransform.sizeDelta.x > rectTransformMaxSize.x;
+            bool yMaxPassed = rectTransform.sizeDelta.y > rectTransformMaxSize.y;
 
             // sets size limit
             // mild bug: might unintentionally move window slightly if resizing too quickly
@@ -209,6 +212,8 @@ public class WindowUIManager : MonoBehaviour, IDragHandler,
 
             }*/
 
+            // fix not being able to stretch horizontal when touching bottom and top bound
+            // and not being able to stretch vertical when touching left and right
             switch (xMinPassed, yMinPassed, xBound, yBound)
             {
                 case (false, false, bound.none, bound.none):
@@ -253,6 +258,26 @@ public class WindowUIManager : MonoBehaviour, IDragHandler,
                     break;
             }
 
+            switch (xMaxPassed, yMaxPassed)
+            {
+                case (true, false):
+                    rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, rectTransformMaxSize.x);
+                    rectTransform.position = new Vector2(Screen.width/2, rectTransform.position.y);
+                    break;
+
+                case (false, true):
+                    rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, rectTransformMaxSize.y);
+                    rectTransform.position = new Vector2(rectTransform.position.x, Screen.height/2);
+                    break;
+
+                case (true, true):
+                    rectTransform.sizeDelta = rectTransformMaxSize;
+                    rectTransform.position = new Vector2(rectTransform.position.x, rectTransform.position.y);
+                    break;
+            }
+
+            // sizeBound(eventData, xMinPassed, yMinPassed, xMaxPassed, yMaxPassed, xBound, yBound);
+
             keepInBounds(xBound, yBound);
         }
     }
@@ -274,9 +299,30 @@ public class WindowUIManager : MonoBehaviour, IDragHandler,
 
     private bound checkXBounds(PointerEventData eventData)
     {
-        bool rightBound = eventData.delta.x + rectTransform.position.x + rectTransform.sizeDelta.x / 2 >= Screen.width;
+        bool rightBound = eventData.delta.x + rectTransform.position.x + rectTransform.sizeDelta.x / 2 > Screen.width;
 
-        bool leftBound = eventData.delta.x + rectTransform.position.x - rectTransform.sizeDelta.x / 2 <= 0;
+        bool leftBound = eventData.delta.x + rectTransform.position.x - rectTransform.sizeDelta.x / 2 < 0;
+
+        switch (resizePoint)
+        {
+            case "left":
+            case "top left corner":
+            case "bottom left":
+                if (rightBound && resizing)
+                {
+                    rightBound = false;
+                }
+                break;
+
+            case "right":
+            case "top right corner":
+            case "bottom right":
+                if (leftBound && resizing)
+                {
+                    leftBound = false;
+                }
+                break;
+        }
 
         if (rightBound)
         {
@@ -292,9 +338,30 @@ public class WindowUIManager : MonoBehaviour, IDragHandler,
 
     private bound checkYBounds(PointerEventData eventData)
     {
-        bool upBound = eventData.delta.y + rectTransform.position.y + rectTransform.sizeDelta.y / 2 >= Screen.height;
+        bool upBound = eventData.delta.y + rectTransform.position.y + rectTransform.sizeDelta.y / 2 > Screen.height;
 
-        bool downBound = eventData.delta.y + rectTransform.position.y - rectTransform.sizeDelta.y / 2 <= 0;
+        bool downBound = eventData.delta.y + rectTransform.position.y - rectTransform.sizeDelta.y / 2 < 0;
+
+        switch (resizePoint)
+        {
+            case "bottom":
+            case "bottom right":
+            case "bottom left":
+                if (upBound && resizing)
+                {
+                    upBound = false;
+                }
+                break;
+
+            case "top resize":
+            case "top right corner":
+            case "top left corner":
+                if(downBound && resizing)
+                {
+                    downBound = false;
+                }
+                break;
+        }
 
         if (upBound)
         {
@@ -353,6 +420,93 @@ public class WindowUIManager : MonoBehaviour, IDragHandler,
                 break;
 
 
+        }
+    }
+
+    private void sizeBound(PointerEventData eventData,
+                           bool xMinPassed,
+                           bool xMaxPassed,
+                           bool yMinPassed,
+                           bool yMaxPassed,
+                           bound xBound,
+                           bound yBound)
+    {
+
+        if(xBound == bound.none && yBound == bound.none)
+        {
+            switch (xMinPassed, yMinPassed, xMaxPassed, yMaxPassed)
+            {
+                // min cases
+                case (true, false, false, false):
+                    rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, rectTransformMinSize.x);
+                    rectTransform.anchoredPosition += (eventData.delta *
+                                                      new Vector2(0, Mathf.Abs(resizeDirection.y)) / 2)
+                                                      / parent.scaleFactor;
+                    break;
+
+                case (false, true, false, false):
+                    rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, rectTransformMinSize.y);
+                    rectTransform.anchoredPosition += (eventData.delta *
+                                                      new Vector2(Mathf.Abs(resizeDirection.x), 0) / 2)
+                                                      / parent.scaleFactor;
+                    break;
+
+                case (true, true, false, false):
+                    rectTransform.sizeDelta = rectTransformMinSize;
+                    break;
+
+
+
+                // max cases
+                case (false, false, true, false):
+                    rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, rectTransformMaxSize.x);
+                    rectTransform.anchoredPosition += (eventData.delta *
+                                                      new Vector2(0, Mathf.Abs(resizeDirection.y)) / 2)
+                                                      / parent.scaleFactor;
+                    break;
+
+                case (false, false, false, true):
+                    rectTransform.SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, rectTransformMaxSize.y);
+                    rectTransform.anchoredPosition += (eventData.delta *
+                                                      new Vector2(Mathf.Abs(resizeDirection.x), 0) / 2)
+                                                      / parent.scaleFactor;
+                    break;
+
+                case (false, false, true, true):
+                    rectTransform.sizeDelta = rectTransformMaxSize;
+                    break;
+
+
+                // normal behavior
+                default:
+                    rectTransform.anchoredPosition += (eventData.delta *
+                                                      new Vector2(Mathf.Abs(resizeDirection.x),
+                                                      Mathf.Abs(resizeDirection.y)) / 2)
+                                                      / parent.scaleFactor;
+                    break;
+            }
+        }
+        else
+        {
+            // stop anchor position from moving
+            switch (xBound, yBound)
+            {
+                case (bound.right, bound.none):
+                case (bound.left, bound.none):
+                    rectTransform.anchoredPosition += (eventData.delta *
+                                                      new Vector2(0,
+                                                      Mathf.Abs(resizeDirection.y)) / 2)
+                                                      / parent.scaleFactor;
+                    break;
+
+                case (bound.none, bound.up):
+                case (bound.none, bound.down):
+                    rectTransform.anchoredPosition += (eventData.delta *
+                                                      new Vector2(Mathf.Abs(resizeDirection.x),
+                                                      0) / 2)
+                                                      / parent.scaleFactor;
+                    break;
+            }
         }
     }
 }
