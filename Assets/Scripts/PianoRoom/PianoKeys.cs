7b07229e256254d38663a84c6f13cf648fb4ba2a
TNCederberg <tim.cederberg16@gmail.com>
2025-04-12 23:16:48 -0500

    Floor Piano + more main changes

---
+++ Assets/Scripts/PianoRoom/PianoKeys.cs
@@ -0,0 +1,62 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PianoKeys : MonoBehaviour
+{
+    private Vector3 originalPosition;
+    private Vector3 downPosition;
+    private Vector3 targetPosition;
+    private bool isPressed = false;
+    private bool hasPlayed = false;
+    private AudioSource audioSource;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = gameObject.GetComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+        originalPosition = transform.localPosition;
+        downPosition = transform.localPosition - new Vector3(0, 0.04f, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isPressed)
+        {
+            targetPosition = downPosition;
+            transform.localPosition = Vector3.Lerp(transform.localPosition, targetPosition, Time.deltaTime * 15f);
+
+
+            if (!hasPlayed && Vector3.Distance(transform.localPosition, targetPosition) < 0.01f)
+            {
+                audioSource.Play();
+                hasPlayed = true;
+            }
+        }
+
+        if (!isPressed)
+        {
+            targetPosition = originalPosition;
+            transform.localPosition = Vector3.Lerp(transform.localPosition, targetPosition, Time.deltaTime * 15f);
+        }
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isPressed = true;
+            hasPlayed = false;
+        }
+    }
+
+    void OnCollisionExit(Collision other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            isPressed = false;
+        }
+    }
+}
