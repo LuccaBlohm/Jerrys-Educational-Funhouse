6d250cec098dfd36403e1ae06f7854b8d28683b2
unavrgjoe <unavrgjoe@gmail.com>
2025-04-14 22:17:48 -0500

    Exit Door Made
    The final glowing exit plane and doorframe are set active when the LightBoard is entirely on.  Also animated the door to swing open. Key Unlock is matthew territory but once implemented itll be ready to go.

--- Assets/Scripts/Interactable Objects/KeyUnlock.cs
+++ Assets/Scripts/Interactable Objects/KeyUnlock.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class KeyUnlock : MonoBehaviour, IInteractable
 {
+    public Animator animator;
+    private bool isOpen = false;
     [SerializeField] private ItemSprite key;
     [SerializeField] private PlayerMovement player;
 
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
     public void OnInteract()
     {
+        isOpen = !isOpen; //door open logic, move into the key check once configured in main
+        animator.SetBool("Open", isOpen);
         if (key == player.itemHeld)
         {
             Debug.Log("Unlock");
-            Destroy(gameObject);
+
         }
     }
 
