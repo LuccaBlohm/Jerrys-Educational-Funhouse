30de159c2ecc09a02a45fc1d8820dd38e08e6bf2
Super-Fun-Inc <edaly@mail.bradley.edu>
2025-04-15 21:28:06 -0500

    Evan GOAT moment

--- Assets/Scripts/PortalsManager.cs
+++ Assets/Scripts/PortalsManager.cs
@@ -6,9 +6,22 @@ public class PortalsManager : MonoBehaviour
 {
     public Camera cameraB;
     public Camera cameraA;
+    public Camera cameraC;
+    public Camera cameraD;
+    public Camera cameraE;
+    public Camera cameraF;
+    public Camera cameraG;
+    public Camera cameraH;
+
 
     public Material materialB;
     public Material materialA;
+    public Material materialC;
+    public Material materialD;
+    public Material materialE;
+    public Material materialF;
+    public Material materialG;
+    public Material materialH;
     // Start is called before the first frame update
     void Start()
     {
@@ -25,6 +38,48 @@ public class PortalsManager : MonoBehaviour
         }
         cameraA.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
         materialA.mainTexture = cameraA.targetTexture;
+
+        if (cameraC.targetTexture != null)
+        {
+            cameraC.targetTexture.Release();
+        }
+        cameraC.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        materialC.mainTexture = cameraC.targetTexture;
+
+        if (cameraD.targetTexture != null)
+        {
+            cameraD.targetTexture.Release();
+        }
+        cameraD.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        materialD.mainTexture = cameraD.targetTexture;
+
+        if (cameraE.targetTexture != null)
+        {
+            cameraE.targetTexture.Release();
+        }
+        cameraE.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        materialE.mainTexture = cameraE.targetTexture;
+
+        if (cameraF.targetTexture != null)
+        {
+            cameraF.targetTexture.Release();
+        }
+        cameraF.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        materialF.mainTexture = cameraF.targetTexture;
+
+        if (cameraG.targetTexture != null)
+        {
+            cameraG.targetTexture.Release();
+        }
+        cameraG.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        materialG.mainTexture = cameraG.targetTexture;
+
+        if (cameraH.targetTexture != null)
+        {
+            cameraH.targetTexture.Release();
+        }
+        cameraH.targetTexture = new RenderTexture(Screen.width, Screen.height, 24);
+        materialH.mainTexture = cameraH.targetTexture;
     }
 
     // Update is called once per frame
