91488a30be89e8bdc2cbd8ae6d0671c1ed62e34f
radical-ish <cruzmatt1015@gmail.com>
2025-04-21 19:18:50 -0500

    code change for wire puzzle
    moved the code for the light effects and animation to the jerry script because i found a bug where you can interrupt it if you close the wire puzzle in the middle of it

--- Assets/Scripts/WirePuzzle/TestJerry.cs
+++ Assets/Scripts/WirePuzzle/TestJerry.cs
@@ -5,16 +5,32 @@ using UnityEngine;
 public class TestJerry : MonoBehaviour
 {
     public Animator jerryAnim;
+    public GameObject statueRoomLights;
 
     // Start is called before the first frame update
     void Start()
     {
-
+        statueRoomLights = GameObject.Find("StatueRoomAfterWirePuzzle");
     }
 
     public void activation()
     {
+        StartCoroutine(lightEffects());
+    }
+
+    private IEnumerator lightEffects()
+    {
+        statueRoomLights.SetActive(true);
+        yield return new WaitForSeconds(0.2f);
+        statueRoomLights.SetActive(false);
+        yield return new WaitForSeconds(0.4f);
+        statueRoomLights.SetActive(true);
+        yield return new WaitForSeconds(0.1f);
+        statueRoomLights.SetActive(false);
+        yield return new WaitForSeconds(1.4f);
+        statueRoomLights.SetActive(true);
         jerryAnim.Play("JerryAnim");
+        StopAllCoroutines();
     }
 
     // Update is called once per frame
