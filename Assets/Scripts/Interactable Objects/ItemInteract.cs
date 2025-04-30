1ab26d9189446f2f9eb43d7a27df291997edb191
radical-ish <cruzmatt1015@gmail.com>
2025-04-30 13:35:26 -0500

    audio system on interactables

--- Assets/Scripts/Interactable Objects/ItemInteract.cs
+++ Assets/Scripts/Interactable Objects/ItemInteract.cs
@@ -4,12 +4,25 @@ public class ItemInteract : MonoBehaviour, IInteractable
 {
     [SerializeField] private ItemSprite key;
     [SerializeField] private PlayerMovement player;
+    [SerializeField] protected AudioSource _rejectSound;
+    [SerializeField] protected AudioSource _acceptSound;
 
     public void OnInteract()
     {
         if (key == player.itemHeld)
         {
+            if (_acceptSound != null)
+            {
+                _acceptSound.Play();
+                _acceptSound = null;
+                _rejectSound = null;
+            }
+
             interactableBehavior();
+        }
+        else if(_rejectSound != null)
+        {
+            _rejectSound.Play();
         }
     }
 
